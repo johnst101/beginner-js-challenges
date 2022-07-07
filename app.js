5ee8c928bfd7012d9f11b28d79976db9050af114
johnst101 <tylercjohnson16@gmail.com>
2022-07-07 07:53:11 -0500

    completed q1

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-function addition
+// BEGINNER PRACTICE QUESTIONS
+
+// Q1: Return the Sum of Two Numbers
+function addition(num1, num2) {
+  return num1 + num2
+}
+
+console.log(addition(3, 2))
+
+// Q2: Convert Hours into Seconds
+
+
+// Q3
+
+// Q4
+
+// Q5
+
+// Q6
+
+// Q7
+
+// Q8
+
+// Q9
+
+// Q10
+
+// Q11

28e430f2b5297c1f47d3924adbec4593015b00e1
johnst101 <tylercjohnson16@gmail.com>
2022-07-10 10:18:57 -0500

    through q9

--- app.js
+++ app.js
@@ -2,28 +2,67 @@
 
 // Q1: Return the Sum of Two Numbers
 function addition(num1, num2) {
-  return num1 + num2
+  return num1 + num2;
 }
 
-console.log(addition(3, 2))
+console.log(addition(3, 2));
 
 // Q2: Convert Hours into Seconds
+function hoursToSeconds(hours) {
+  return hours * 60 * 60;
+}
 
+console.log(hoursToSeconds(2));
 
-// Q3
+// Q3: Calculate the Perimeter of a Rectangle
+function calcPerimeter(length, width) {
+  return 2 * (length + width);
+}
 
-// Q4
+console.log(calcPerimeter(6, 7));
 
-// Q5
+// Q4: Calculate the Area of a Triangle
+function calcTriangleArea(base, height) {
+  return 0.5 * base * height;
+}
 
-// Q6
+console.log(calcTriangleArea(3, 2));
 
-// Q7
+// Q5: Extend a String
+function appendFrontend(string) {
+  return string + "Frontend";
+}
 
-// Q8
+console.log(appendFrontend("Apple"));
 
-// Q9
+// Q6: Greater than 100?
+function sumGreaterThan100(num3, num4) {
+  return num3 + num4 > 100;
+}
 
-// Q10
+console.log(sumGreaterThan100(50, 60));
 
-// Q11
+// Q7: Less than or Equal to Zero?
+function lessThanOrEqualToZero(num5) {
+  return num5 <= 0;
+}
+
+console.log(lessThanOrEqualToZero(3));
+
+// Q8: Opposite Boolen
+function oppositeBoolean(bool) {
+  return !bool;
+}
+
+console.log(oppositeBoolean(false));
+
+// Q9: Is not the number 0
+function isNotZero(element) {
+return element !== 0;
+}
+
+console.log(isNotZero(5));
+
+// Q10: Calculate the Remainder
+
+// Q11:
